1064ace9a3326416bc4b5d29bfd3ea7957812f41
Danila <dobrotdanila@gmail.com>
2020-09-10 19:39:15 +0300

    Initial Commit

---
+++ Tinkoff Fintech App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinkoff Fintech App
+//
+//  Created by Данила on 10.09.2020.
+//  Copyright © 2020 Dobrotolyubov Danila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
