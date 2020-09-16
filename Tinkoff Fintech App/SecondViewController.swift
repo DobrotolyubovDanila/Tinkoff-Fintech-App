0d0cfc867827b7d0432f227cf95da5df593c030a
Данила <danon@MacBook-Pro-Danila.local>
2020-09-16 16:00:06 +0300

    Add .gitignore file

---
+++ Tinkoff Fintech App/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  Tinkoff Fintech App
+//
+//  Created by Данила on 13.09.2020.
+//  Copyright © 2020 Dobrotolyubov Danila. All rights reserved.
+//
+
+import Foundation
